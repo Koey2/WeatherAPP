6fb3a344223bea08a2cdbb80cb2b5e64b5fe7261
Koey2 <76500599+Koey2@users.noreply.github.com>
2021-03-15 20:40:50 +0100

    Add files via upload

---
+++ weather/weatherapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import City
+
+class CityForm(forms.ModelForm):
+    class Meta:
+        model = City
+        fields = ("name",)
+        widgets = {'name': forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Type city',
+            'type': 'text'
+            })}
